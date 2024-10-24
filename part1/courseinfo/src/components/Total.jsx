503fcce46839f0bd24a03de462319ac3497fc2b0
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-24 12:10:44 +0200

    exercise 1.5 completed

--- part1/courseinfo/src/components/Total.jsx
+++ part1/courseinfo/src/components/Total.jsx
@@ -1,5 +1,8 @@
-const Total = ({ total }) => {
-  return <p>Number of exercises {total}</p>
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+  return (
+    <p>Number of exercises {total}</p>
+  )
 }
 
 export default Total
