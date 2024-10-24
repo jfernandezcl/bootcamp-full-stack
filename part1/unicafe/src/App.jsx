252b7a6edd2f7b219a9cbe339827b7ac0fa7d017
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-25 00:04:24 +0200

    exercise 1.7 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 const App = () => {
-  // guarda los clics de cada botón en su propio estado
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -9,6 +9,10 @@ const App = () => {
   const handleGoodClick = () => setGood(good + 1)
   const handleNeutralClick = () => setNeutral(neutral + 1)
   const handleBadClick = () => setBad(bad + 1)
+
+  const total = good + neutral + bad
+  const average = total > 0 ? (good - bad) / total : 0
+  const positive = total > 0 ? (good / total) * 100 : 0
 
   return (
     <div>
@@ -20,6 +24,10 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
+
 
     </div>
   )
