f81a21782fe0fadb74be72b48b70b60eb2ea25be
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-26 00:22:29 +0200

    exercise 2.2 completed

--- part2/exercises-2.1-2.5/src/components/Course.jsx
+++ part2/exercises-2.1-2.5/src/components/Course.jsx
@@ -1,11 +1,17 @@
 import Header from './Header'
 import Content from './Content'
 
-const Course = ({ course }) => (
-  <div>
-    <Header courseName={course.name} />
-    <Content parts={course.parts} />
-  </div>
-)
+const Course = ({ course }) => {
+
+  const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0)
+
+  return (
+    <div>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <p><strong>Total of {totalExercises} exercises</strong></p>
+    </div>
+  )
+}
 
 export default Course
