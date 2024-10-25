df7d260785473536eacbc8aa89a95a7dac210a4a
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-26 00:03:42 +0200

    exercise 2.1 completed

---
+++ part2/exercises-2.1-2.5/src/components/Course.jsx
@@ -0,0 +1,11 @@
+import Header from './Header'
+import Content from './Content'
+
+const Course = ({ course }) => (
+  <div>
+    <Header courseName={course.name} />
+    <Content parts={course.parts} />
+  </div>
+)
+
+export default Course
