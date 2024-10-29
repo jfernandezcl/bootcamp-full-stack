c5154453cd61730c3050f5cc49e1487ab8d6b337
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-29 18:03:38 +0100

    exercise 2.19 completed

--- part2/country-data/src/App.jsx
+++ part2/country-data/src/App.jsx
@@ -6,6 +6,7 @@ import CountryList from "./components/CountryList";
 function App() {
   const [countries, setCountries] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
+  const [selectedCountry, setSelectedCountry] = useState(null)
 
   const handleSearch = async (input) => {
     setSearchTerm(input)
@@ -13,11 +14,13 @@ function App() {
       try {
         const response = await axios.get(`https://restcountries.com/v3.1/name/${input}`)
         setCountries(response.data)
+        setSelectedCountry(null)
       } catch (error) {
         setCountries([])
       }
     } else {
       setCountries([])
+      setSelectedCountry(null)
     }
   }
 
@@ -25,7 +28,7 @@ function App() {
     <div>
       <h1>Country Finder</h1>
       <Search onSearch={handleSearch} />
-      <CountryList countries={countries} />
+      <CountryList countries={countries} selectedCountry={selectedCountry} setSelectedCountry={setSelectedCountry} />
     </div>
   )
 }
--- part2/country-data/src/components/CountryList.jsx
+++ part2/country-data/src/components/CountryList.jsx
@@ -1,17 +1,31 @@
-const CountryList = ({ countries }) => {
+const CountryList = ({ countries, selectedCountry, setSelectedCountry }) => {
+
   if (countries.length > 10) {
     return <p>Too many results</p>
   }
 
-  if (countries.length === 1) {
-    const country = countries[0]
+  if (selectedCountry) {
     return (
       <div>
-        <h2>{country.name.common}</h2>
-        <img src={country.flags.png} alt={`Flag of ${country.name.common}`} />
-        <p>Capital: {country.capital}</p>
-        <p>Area: {country.area} km</p>
-        <p>Languages: {Object.values(country.languages).join(', ')}</p>
+        <h2>{selectedCountry.name.common}</h2>
+        <img src={selectedCountry.flags.png} alt={`Flag of ${selectedCountry.name.common}`} />
+        <p>Capital: {selectedCountry.capital}</p>
+        <p>Area: {selectedCountry.area} km</p>
+        <p>Languages: {Object.values(selectedCountry.languages).join(', ')}</p>
+        <button onClick={() => setSelectedCountry(null)}>Back to the list</button>
+      </div>
+    )
+  }
+
+  if (countries.length === 1) {
+    const singleCountry = countries[0]
+    return (
+      <div>
+        <h2>{singleCountry.name.common}</h2>
+        <img src={singleCountry.flags.png} alt={`Flag of ${singleCountry.name.common}`} />
+        <p>Capital: {singleCountry.capital}</p>
+        <p>Area: {singleCountry.area} km</p>
+        <p>Languages: {Object.values(singleCountry.languages).join(', ')}</p>
       </div>
     )
   }
@@ -19,7 +33,10 @@ const CountryList = ({ countries }) => {
   return (
     <ul>
       {countries.map((country) => (
-        <li key={country.cca3}>{country.name.common}</li>
+        <li key={country.cca3}>
+          {country.name.common}
+          <button onClick={() => setSelectedCountry(country)}>Show</button>
+        </li>
       ))}
     </ul>
   )
