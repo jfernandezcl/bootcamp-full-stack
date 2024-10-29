b2a70c9b6228a7f970d4d5f528b491f61d819d71
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-29 18:27:05 +0100

    exercise 2.20 completed

--- part2/country-data/src/components/CountryList.jsx
+++ part2/country-data/src/components/CountryList.jsx
@@ -1,4 +1,22 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 const CountryList = ({ countries, selectedCountry, setSelectedCountry }) => {
+  const [weather, setWeather] = useState(null)
+
+  useEffect(() => {
+    if (selectedCountry) {
+      const capital = selectedCountry.capital[0]
+      axios
+        .get(`https://www.el-tiempo.net/api/json/v2/municipio/estado/${capital}`)
+        .then((response) => {
+          setWeather(response.data)
+        })
+        .catch((error) => {
+          console.error("Error fetching weather data:", error)
+        })
+    }
+  }, [selectedCountry])
 
   if (countries.length > 10) {
     return <p>Too many results</p>
@@ -12,6 +30,18 @@ const CountryList = ({ countries, selectedCountry, setSelectedCountry }) => {
         <p>Capital: {selectedCountry.capital}</p>
         <p>Area: {selectedCountry.area} km</p>
         <p>Languages: {Object.values(selectedCountry.languages).join(', ')}</p>
+
+        {weather ? (
+          <div>
+            <h3>Weather in {selectedCountry.capital[0]}</h3>
+            <p>State: {weather.estado}</p>
+            <p>Temperature: {weather.temperatura} °C</p>
+            <p>Condition: {weather.estadoTiempo}</p>
+            <img src={weather.iconoTiempo} alt={weather.estadoTiempo} />
+          </div>
+        ) : (
+          <p>Loading weather data...</p>
+        )}
         <button onClick={() => setSelectedCountry(null)}>Back to the list</button>
       </div>
     )
@@ -26,6 +56,7 @@ const CountryList = ({ countries, selectedCountry, setSelectedCountry }) => {
         <p>Capital: {singleCountry.capital}</p>
         <p>Area: {singleCountry.area} km</p>
         <p>Languages: {Object.values(singleCountry.languages).join(', ')}</p>
+        <button onClick={() => setSelectedCountry(null)}>Back to the list</button>
       </div>
     )
   }
