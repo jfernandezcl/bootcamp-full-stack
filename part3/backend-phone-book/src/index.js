f4427689dd58ca0845751c9f522d0ec3fb0e14e8
jfernandezcl <javierfernandez0987@gmail.com>
2024-11-03 00:20:37 +0100

    exercise 3.8 completed

--- part3/backend-phone-book/src/index.js
+++ part3/backend-phone-book/src/index.js
@@ -5,7 +5,12 @@ const morgan = require('morgan')
 const personsRouter = require('./routes/persons');
 
 app.use(express.json());
-app.use(morgan('tiny'))
+
+morgan.token('body', (req) => {
+  return req.method === 'POST' ? JSON.stringify(req.body) : ''
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.use('/api/persons', personsRouter);
 
