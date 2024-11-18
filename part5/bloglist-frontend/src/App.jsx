7c47a84a508c1e768d032e71d2804a0d07b44bbf
jfernandezcl <javierfernandez0987@gmail.com>
2024-11-18 12:13:09 +0100

    exercise 5.1 completed

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,22 +1,47 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState('')
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    if (user) {
+      blogService.getAll().then(blogs => setBlogs(blogs))
+    }
+  }, [user])
+
+  const handleLogin = async (credentials) => {
+    try {
+      const loggedInUser = await loginService.login(credentials)
+      setUser(loggedInUser)
+      setErrorMessage('')
+    } catch (error) {
+      setErrorMessage('Invalid username or password')
+      setTimeout(() => setErrorMessage(''), 5000)
+    }
+  }
+
+  if (!user) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+        <LoginFrom handleLogin={handleLogin} />
+      </div>
+    )
+  }
 
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      <p>Logged in as {user.name}</p>
+      {blogs.map(blog => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
   )
 }
