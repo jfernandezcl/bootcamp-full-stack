00faccaa15b92e47190c8c175f30c7976fd64677
jfernandezcl <javierfernandez0987@gmail.com>
2024-11-18 13:13:50 +0100

    exercise 5.2 completed

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -9,21 +9,39 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState('')
 
   useEffect(() => {
+    const loggedUserJSON = localStorage.getItem('loggedBlogUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  useEffect(() => {
     if (user) {
       blogService.getAll().then(blogs => setBlogs(blogs))
     }
   }, [user])
 
+
   const handleLogin = async (credentials) => {
     try {
       const loggedInUser = await loginService.login(credentials)
+      localStorage.setItem('loggedBlogUser', JSON.stringify(loggedInUser))
       setUser(loggedInUser)
+      blogService.setToken(loggedInUser.token)
       setErrorMessage('')
     } catch (error) {
       setErrorMessage('Invalid username or password')
       setTimeout(() => setErrorMessage(''), 5000)
     }
   }
+
+  const handleLogout = () => {
+    localStorage.removeItem('loggedBlogUser')
+    setUser(null)
+  }
+
 
   if (!user) {
     return (
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,18 @@
 import axios from 'axios'
-const baseUrl = '/api/blogs'
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+const baseUrl = '/api/blogs'
+let token = null
+
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`
 }
 
-export default { getAll }
+const getAll = async () => {
+  const config = {
+    headers: { authorization: token },
+  }
+  const response = await axios.get(baseUrl, config)
+  return response.data
+}
+
+export default { getAll, setToken }
