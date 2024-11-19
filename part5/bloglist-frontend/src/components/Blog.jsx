ac834d5b3cc30147a27da4f2aa5323468a2f3f4c
jfernandezcl <javierfernandez0987@gmail.com>
2024-11-19 19:33:44 +0100

    exercise 5.7 completed

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false)
+
+  const toggleDetails = () => setDetailsVisible(!detailsVisible)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleDetails}>
+          {detailsVisible ? 'Hide' : 'View'}
+        </button>
+      </div>
+      {detailsVisible && (
+        <div>
+          <p>URL: {blog.url}</p>
+          <p>Likes: {blog.likes} <button>Like</button></p>
+          <p>Added by: {blog.user?.name || 'Unknown'}</p>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
