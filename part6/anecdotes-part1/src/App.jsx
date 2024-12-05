fa6bfd3df2ab422053d1e84878c0d7f4d1d8187d
jfernandezcl <javierfernandez0987@gmail.com>
2024-12-05 12:38:46 +0100

    exercise 6.14 completed

--- part6/anecdotes-part1/src/App.jsx
+++ part6/anecdotes-part1/src/App.jsx
@@ -1,17 +1,22 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
-import Filter from './components/Filter'
-import Notification from './components/Notification'
-
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+
+  useEffect(() => {
+    dispatch(initializeAnecdotes())
+  }, [dispatch])
+
   return (
     <div>
-      <h2>Anecdotes</h2>
-      <Notification />
-      <Filter />
-      <AnecdoteList />
+      <h1>Software anecdotes</h1>
       <AnecdoteForm />
+      <AnecdoteList />
     </div>
   )
 }
--- part6/anecdotes-part1/src/reducers/anecdoteReducer.js
+++ part6/anecdotes-part1/src/reducers/anecdoteReducer.js
@@ -1,26 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { setTimedNotification } from '../notificationReducer'
-
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+import axios from 'axios'
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
   return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
+    content: anecdote.content,
+    id: anecdote.id,
+    votes: anecdote.votes,
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
+const initialState = []
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
@@ -42,10 +34,22 @@ const anecdoteSlice = createSlice({
       }
       return [...state, newAnecdote]
     },
+    setAnecdotes(state, action) {
+      return action.payload
+    }
   },
 })
 
-export const { voteAnecdote, createAnecdote } = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const response = await axios.get('http://localhost:3001/anecdotes')
+    const anecdotes = response.data.map(asObject)
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+
+export const { voteAnecdote, createAnecdote, setAnecdotes } = anecdoteSlice.actions
 
 export const handleVote = (id) => {
   return (dispatch, getState) => {
