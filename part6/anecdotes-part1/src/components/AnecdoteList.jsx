ef4edf16c205de37badb300ebe2944e5afd3ffcd
jfernandezcl <javierfernandez0987@gmail.com>
2024-12-06 20:32:08 +0100

    exercise 6.20 completed

--- part6/anecdotes-part1/src/components/AnecdoteList.jsx
+++ part6/anecdotes-part1/src/components/AnecdoteList.jsx
@@ -1,14 +1,40 @@
 import { useSelector, useDispatch } from 'react-redux'
+import { useQuery } from '@tanstack/react-query'
 import { handleVote } from '../reducers/anecdoteReducer'
+import { getAnecdotes } from '../services/anecdoteService'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(({ anecdotes, filter }) =>
-    anecdotes.filter((a) => a.content.toLowerCase().includes(filter.toLowerCase()))
-  )
-
   const dispatch = useDispatch()
 
-  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes)
+  const { data: anecdotes, isLoading, isError, error } = useQuery(
+    ['anecdotes'],
+    getAnecdotes,
+    {
+      retry: 1,
+    }
+  )
+
+  const filter = useSelector((state) => state.filter)
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h2>Error</h2>
+        <p>Unable to fetch anecdotes. The anecdote service is unavailable.</p>
+        <p>{error.message}</p>
+      </div>
+    )
+  }
+
+  const filteredAnecdotes = anecdotes.filter((a) =>
+    a.content.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  const sortedAnecdotes = [...filteredAnecdotes].sort((a, b) => b.votes - a.votes)
 
   return (
     <div>
@@ -27,3 +53,4 @@ const AnecdoteList = () => {
 }
 
 export default AnecdoteList
+
