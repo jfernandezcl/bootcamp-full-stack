57da4b1d09ecaa75affbb93981cedfb16783d80d
jfernandezcl <javierfernandez0987@gmail.com>
2024-12-17 18:03:39 +0100

    exercise 7.17 completed

--- part7/expand-blog-list/src/App.jsx
+++ part7/expand-blog-list/src/App.jsx
@@ -105,7 +105,22 @@ const App = () => {
         </p>
 
         <nav>
-          <Link to="/users">Users</Link>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+            <li>
+              <Link to="/blogs">Blogs</Link>
+            </li>
+            {user && (
+              <li>
+                <button onClick={handleLogout}>Logout</button>
+              </li>
+            )}
+          </ul>
         </nav>
 
         <Routes>
