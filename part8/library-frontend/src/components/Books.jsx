f98e53b4c0f9372446b1bf932257a54e02746246
jfernandezcl <javierfernandez0987@gmail.com>
2024-12-19 16:37:13 +0100

    exercise 8.9 completed

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -1,9 +1,13 @@
 const Books = (show) => {
-  if (show) {
+  if (!show) {
     return null
   }
 
-  const books = []
+  const books = [
+    { title: "Harry Potter and the Sorcerer's Stone", author: "J.K. Rowling", published: 1997 },
+    { title: "A Game of Thrones", author: "George R.R. Martin", published: 1996 },
+    { title: "The Hobbit", author: "J.R.R. Tolkien", published: 1937 },
+  ]
 
   return (
     <div>
