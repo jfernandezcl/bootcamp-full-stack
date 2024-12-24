0c93f9c8efb6b67df670d5d59d3367d7aa6e67a9
jfernandezcl <javierfernandez0987@gmail.com>
2024-12-24 19:55:01 +0100

    exercise 9.9 completed

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,12 +1,16 @@
-import express from 'express'
+import express from 'express';
+import cors from 'cors';
 
-const app = express()
-const PORT = 30001
+const app = express(); 
+
+app.use(cors()); 
+
+const PORT = 30001;
 
 app.get('/api/ping', (_req, res) => {
-  res.send('pong')
-})
+  res.send('pong');
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
-})
+});
