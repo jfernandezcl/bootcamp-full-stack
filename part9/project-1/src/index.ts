fd7dc810a348d92b4131ba42b281529255dd0d5c
jfernandezcl <javierfernandez0987@gmail.com>
2024-12-24 16:54:56 +0100

    exercise 9.6 completed

--- part9/project-1/src/index.ts
+++ part9/project-1/src/index.ts
@@ -4,23 +4,23 @@ import { calculateBmi } from './bmiCalculator';
 const app = express();
 const PORT = 3002;
 
-app.get('/hello', (_req: Request, res: Response) => {
+app.get('/hello', (_req: Request, res: Response): void => {
   res.send('Hello Full Stack!');
 });
 
-app.get('/bmi', (req: Request, res: Response) => {
+app.get('/bmi', (req: Request, res: Response): void => {
   const height = Number(req.query.height);
   const weight = Number(req.query.weight);
 
   if (!height || !weight || isNaN(height) || isNaN(weight)) {
     res.status(400).json({ error: 'malformatted parameters' });
-    return; // Para garantizar que no haya rutas sin valor de retorno.
+    return;
   }
 
   const bmi = calculateBmi(height, weight);
   res.json({ weight, height, bmi });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, (): void => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
