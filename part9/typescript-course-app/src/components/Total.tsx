129f7075a705787472396d91714faf76ad5708fe
jfernandezcl <javierfernandez0987@gmail.com>
2024-12-27 17:09:25 +0100

    exercise 9.14 completed

---
+++ part9/typescript-course-app/src/components/Total.tsx
@@ -0,0 +1,15 @@
+interface CoursePart {
+  name: string;
+  exerciseCount: number;
+}
+
+interface TotalProps {
+  courseParts: CoursePart[];
+}
+
+const Total = ({ courseParts }: TotalProps) => {
+  const totalExercises = courseParts.reduce((sum, part) => sum + part.exerciseCount, 0);
+  return <p>Number of exercises {totalExercises}</p>;
+};
+
+export default Total;
